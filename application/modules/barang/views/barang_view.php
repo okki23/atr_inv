c6c81717bfda29d0b6c1e42ec3ee9cce26cdf035
okki23 <okkisetyawan@gmail.com>
2020-05-26 13:38:20 +0700

    fix barang

--- application/modules/barang/views/barang_view.php
+++ application/modules/barang/views/barang_view.php
@@ -34,14 +34,11 @@
                         </div>
                     </div>
                 </div>
-            </div>
-         
-
+            </div> 
 
         </div>
-    </section>
+    </section> 
 
-   
 	<!-- form tambah dan ubah data -->
 	<div class="modal fade" id="defaultModal" tabindex="-1" role="dialog">
                 <div class="modal-dialog" role="document">
@@ -50,16 +47,13 @@
                             <h4 class="modal-title" id="defaultModalLabel">Form Tambah Data</h4>
                         </div>
                         <div class="modal-body">
-                              <form method="post" id="user_form" enctype="multipart/form-data">   
-                                 
-									<input type="hidden" name="id" id="id"> 
- 
+                              <form method="post" id="user_form" enctype="multipart/form-data">    
+									<input type="hidden" name="id" id="id">  
                                     <div class="form-group">
                                         <div class="form-line">
                                             <input type="text" name="kode_barang" id="kode_barang" class="datepicker form-control" placeholder="Kode Barang" />
                                         </div>
-                                    </div>
-                                    
+                                    </div> 
                                     <div class="form-group">
                                         <div class="form-line">
                                             <input type="text" name="nama_barang" id="nama_barang" class="form-control" placeholder="Nama Barang" />
@@ -67,20 +61,44 @@
                                     </div>
                                     <div class="form-group">
                                         <div class="form-line">
-                                            <input type="text" name="qty_jkt" id="qty_jkt" class="form-control" placeholder="Qty Jakarta" />
+                                            <input type="text" name="merk_model" id="merk_model" class="form-control" placeholder="Merk Model" />
                                         </div>
                                     </div>
                                     <div class="form-group">
                                         <div class="form-line">
-                                            <input type="text" name="qty_subang" id="qty_subang" class="form-control" placeholder="Qty Subang" />
+                                            <input type="text" name="no_serial_pabrik" id="no_serial_pabrik" class="form-control" placeholder="No Serial Pabrik" />
                                         </div>
                                     </div>
                                     <div class="form-group">
                                         <div class="form-line">
-                                            <input type="text" name="keterangan" id="keterangan" class="form-control" placeholder="Keterangan" />
+                                            <input type="text" name="ukuran" id="ukuran" class="form-control" placeholder="Ukuran" />
                                         </div>
                                     </div>
-                                     
+									<div class="form-group">
+                                        <div class="form-line">
+                                            <input type="text" name="bahan" id="bahan" class="form-control" placeholder="Bahan" />
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <div class="form-line">
+                                            <input type="text" name="tahun_buat" id="tahun_buat" class="form-control" placeholder="Tahun Buat" />
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <div class="form-line">
+                                            <input type="text" name="jumlah_barang" id="jumlah_barang" class="form-control" placeholder="Jumlah Barang" />
+                                        </div>
+                                    </div>
+									<div class="form-group">
+                                        <div class="form-line">
+                                            <input type="text" name="satuan_barang" id="satuan_barang" class="form-control" placeholder="Satuan Barang" />
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <div class="form-line">
+                                            <input type="text" name="harga_beli" id="harga_beli" class="form-control" placeholder="Harga Beli" />
+                                        </div>
+                                    </div> 
 
 								   <button type="button" onclick="Simpan_Data();" class="btn btn-success waves-effect"> <i class="material-icons">save</i> Simpan</button>
 
@@ -90,72 +108,7 @@
                      
                     </div>
                 </div>
-    </div>
-
-
-
-    <!-- modal cari kategori -->
-    <div class="modal fade" id="CariKategoriModal" tabindex="-1" role="dialog">
-                <div class="modal-dialog modal-lg" role="document">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h4 class="modal-title" >Cari Kategori</h4>
-                        </div>
-                        <div class="modal-body">
-                                <button type="button" class="btn btn-danger" data-dismiss="modal">X Tutup</button>
-
-                                <br>
-                                <hr>
-
-                                 <table width="100%" class="table table-bordered table-striped table-hover " id="daftar_kategori" >
-  
-                                    <thead>
-                                        <tr>  
-                                            <th style="width:98%;">Kategori </th> 
-                                         </tr>
-                                    </thead> 
-                                    <tbody id="daftar_kategorix">
-
-                                </tbody>
-                                </table> 
-                       </div>
-                     
-                    </div>
-                </div>
-    </div>
-
-
-    <!-- modal cari subkategori -->
-    <div class="modal fade" id="CariSubKategoriModal" tabindex="-1" role="dialog">
-                <div class="modal-dialog modal-lg" role="document">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h4 class="modal-title" >Cari Sub Kategori</h4>
-                        </div>
-                        <div class="modal-body">
-                                <button type="button" class="btn btn-danger" data-dismiss="modal">X Tutup</button>
-
-                                <br>
-                                <hr>
-
-                                 <table width="100%" class="table table-bordered table-striped table-hover " id="tabel_sub_kategori" > 
-                                    <thead>
-                                        <tr>  
-                                            <th style="width:15%;">Nama Sub Kategori</th> 
-                                            <th style="width:15%;">Action</th> 
-                                         </tr>
-                                    </thead>
-                                    <tbody>
-                                        
-                                    </tbody>  
-                                </table>  
-                       </div>
-                     
-                    </div>
-                </div>
-    </div>
-
-    
+    </div> 
      <!-- modal detail -->
      <div class="modal fade" id="DetailModal" tabindex="-1" role="dialog">
                 <div class="modal-dialog modal-lg" role="document">
@@ -169,53 +122,69 @@
                                 <hr>
                                 <table class="table table-bordered table-hovered">
                                 <tr>
+                                    <td>Kode Barang</td>
+                                    <td>:</td>
+                                    <td><div id="kode_barangdtl"> </div></td>
+                                </tr>
+								<tr>
                                     <td>Nama Barang</td>
                                     <td>:</td>
                                     <td><div id="nama_barangdtl"> </div></td>
                                 </tr>
                                 <tr>
-                                    <td>Kategori Barang</td>
+                                    <td>Merk Model</td>
                                     <td>:</td>
-                                    <td><div id="nama_kategoridtl"> </div></td>
+                                    <td><div id="merk_modeldtl"> </div></td>
                                 </tr>
                                 <tr>
-                                    <td>Sub Kategori Barang</td>
+                                    <td>No Serial Pabrik</td>
                                     <td>:</td>
-                                    <td><div id="nama_sub_kategoridtl"> </div></td>
+                                    <td><div id="no_serial_pabrikdtl"> </div></td>
                                 </tr>
                                 <tr>
-                                    <td>Qty Jakarta</td>
+                                    <td>Ukuran</td>
                                     <td>:</td>
-                                    <td><div id="qty_jktdtl"> </div></td>
+                                    <td><div id="ukurandtl"> </div></td>
                                 </tr>
                                 <tr>
-                                    <td>Qty Subang</td>
+                                    <td>Bahan</td>
                                     <td>:</td>
-                                    <td><div id="qty_subangdtl"> </div></td>
+                                    <td><div id="bahandtl"> </div></td>
                                 </tr>
                                 <tr>
-                                    <td>Keterangan</td>
+                                    <td>Tahun Buat</td>
                                     <td>:</td>
-                                    <td><div id="keterangandtl"> </div></td>
+                                    <td><div id="tahun_buatdtl"> </div></td>
                                 </tr>
-                                
-                                </table>
-                                 
-                       </div>
-                     
+								<tr>
+                                    <td>No Kode Barang</td>
+                                    <td>:</td>
+                                    <td><div id="no_kode_barangdtl"> </div></td>
+                                </tr>
+								<tr>
+                                    <td>Jumlah Barang</td>
+                                    <td>:</td>
+                                    <td><div id="jumlah_barangdtl"> </div></td>
+                                </tr>
+								<tr>
+                                    <td>Satuan Barang</td>
+                                    <td>:</td>
+                                    <td><div id="satuan_barangdtl"> </div></td>
+                                </tr>
+								<tr>
+                                    <td>Harga Beli</td>
+                                    <td>:</td>
+                                    <td><div id="harga_belidtl"> </div></td>
+                                </tr> 
+                                </table> 
+                       </div> 
                     </div>
                 </div>
-    </div>
-
-
-
- 
+    </div> 
 
   
  
-   <script type="text/javascript"> 
-     
-
+   <script type="text/javascript">  
     function Detail(id){
         $("#DetailModal").modal({backdrop: 'static', keyboard: false,show:true});
  
@@ -223,16 +192,18 @@
 			 url:"<?php echo base_url(); ?>barang/get_data_edit/"+id,
 			 type:"GET",
 			 dataType:"JSON", 
-			 success:function(result){ 
-                  
-				 //$("#DetailModal").modal('show'); 
-				 $("#nama_barangdtl").html(result.nama_barang);
-                 $("#nama_kategoridtl").html(result.nama_kategori);
-                 $("#nama_sub_kategoridtl").html(result.nama_sub_kategori); 
-                 $("#qty_jktdtl").html(result.qty_jkt);
-                 $("#qty_subangdtl").html(result.qty_subang);
-                 $("#keterangandtl").html(result.keterangan);  
-                
+			 success:function(result){  
+				 $("#kode_barangdtl").html(result.kode_barang);
+                 $("#nama_barangdtl").html(result.nama_barang);
+                 $("#merk_modeldtl").html(result.merk_model); 
+                 $("#no_serial_pabrikdtl").html(result.no_serial_pabrik);
+                 $("#ukurandtl").html(result.ukuran);
+                 $("#bahandtl").html(result.bahan); 
+				 $("#tahun_buatdtl").html(result.tahun_buat);
+                 $("#no_kode_barangdtl").html(result.no_kode_barang);
+                 $("#jumlah_barangdtl").html(result.jumlah_barang); 
+                 $("#satuan_barangdtl").html(result.satuan_barang);
+                 $("#harga_belidtl").html(result.harga_beli); 
 			 }
 		 });
     } 
@@ -248,16 +219,17 @@
 			 success:function(result){  
 				 $("#defaultModal").modal('show'); 
 				 $("#id").val(result.id);
+				 $("#kode_barang").val(result.kode_barang);
                  $("#nama_barang").val(result.nama_barang);
-                 $("#qty_jkt").val(result.qty_jkt);
-                 $("#qty_subang").val(result.qty_subang);
-                 $("#qty").val(result.qty);
-                 $("#keterangan").val(result.keterangan); 
-                 $("#id_kategori").val(result.id_kategori);
-                 $("#nama_kategori").val(result.nama_kategori); 
-                 $("#id_sub_kategori").val(result.id_sub_kategori);
-                 $("#nama_sub_kategori").val(result.nama_sub_kategori); 
-                  
+                 $("#merk_model").val(result.merk_model); 
+                 $("#no_serial_pabrik").val(result.no_serial_pabrik);
+                 $("#ukuran").val(result.ukuran);
+                 $("#bahan").val(result.bahan); 
+				 $("#tahun_buat").val(result.tahun_buat);
+                 $("#no_kode_barang").val(result.no_kode_barang);
+                 $("#jumlah_barang").val(result.jumlah_barang); 
+                 $("#satuan_barang").val(result.satuan_barang);
+                 $("#harga_beli").val(result.harga_beli); 
 			 }
 		 });
 	 }
